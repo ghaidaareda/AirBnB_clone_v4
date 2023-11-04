d5340b10f0fe7f8816bc4d39b474f03e4929fa3c
jailan-sh <jilan.elsherbiny@gmail.com>
2023-11-04 17:00:36 +0200

    push

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,11 @@
 $("document").ready(funcrion(){
-    
-})
+    const amId = {};
+    $('input[type=checkbox]').change(function(){
+        if (this.checked) {
+            amId[$(this).attr('data-id')] = $(this).attr('data-name');
+          } else {
+            delete amId[$(this).attr('data-id')];}
+        });
+        const names = Object.keys(amId);
+        $('.amenities h4').text(names.sort().join(', '));
+});s
